d79490f30237fccf2674fe47c95f3f66324dbe74
tjdwo1027 <james6299@naver.com>
2023-04-27 16:42:14 +0900

    ADD COMMIT
    9주차

--- src/index.js
+++ src/index.js
@@ -10,12 +10,14 @@ import Accommodate from './chapter_07/Accommodate';
 
 
 
-const root = ReactDOM.createRoot(document.getElementById(`root`));
-root.render(
-  <React.StrictMode>
-    <Accommodate />
-  </React.StrictMode>
-)
+
+
+// const root = ReactDOM.createRoot(document.getElementById(`root`));
+// root.render(
+//   <React.StrictMode>
+//     <Accommodate />
+//   </React.StrictMode>
+// )
 
 // const root = ReactDOM.createRoot(document.getElementById(`root`));
 // root.render(
